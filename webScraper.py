28467b0f802fe27fdd8cb913e59d5c7b54f00954
Anushka-Tiwari2006 <79465532+Anushka-Tiwari2006@users.noreply.github.com>
2022-05-28 21:41:49 +0530

    Add files via upload

---
+++ webScraper.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
+
+page = requests.get(bright_stars_url)
+
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+Star_names = []
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    
+df2 = pd.DataFrame(list(zip(Star_names)),columns=['Star_name'])
+df2.to_csv('stars.csv')
